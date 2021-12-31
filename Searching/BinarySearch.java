9c96b71fe8e69a7e266aa3c4ddddbedfe23fcfc6
Samit Das <40141963+samitdas2003@users.noreply.github.com>
2021-12-31 16:31:39 +0530

    Update BinarySearch.java

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,2 +1,36 @@
-public static int binarySearch(int arr[], int val) {}
+public class Searching {
+	
+	// Iterating
+	public static int binarySearch(int arr[], int x) {
+		int low = 0;
+		int hi = arr.length-1;
+		while(hi >= low) {
+			int mid = (hi + low)/2;
+			if(x == arr[mid]) {
+				return mid;
+			} else if(x > arr[mid]) {
+				low = mid + 1;
+			} else {
+				hi = mid - 1;
+			}
+		}
+		return -1;
+	}
+	
+	// Recursive
+	public static int binarySearchRecursive(int arr[], int low, int hi, int x) {
+		if(low > hi) {
+			return -1;
+		}
+		int mid = (hi + low)/2;
+		if(x == arr[mid]) {
+			return mid;
+		} else if(x > arr[mid]) {
+			return binarySearchRecursive(int arr[], mid+1, hi, x);
+		} else {
+			return binarySearchRecursive(int arr[], low, mid-1, x);
+		}
+	}
+	
+}
 	
