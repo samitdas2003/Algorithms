024bfa88d17b4b5e36338f1e3c5c7c6fdc914296
Samit Das <40141963+samitdas2003@users.noreply.github.com>
2022-01-04 19:03:07 +0530

    Update SelectionSort.java

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,5 +1,17 @@
 public class SelectionSorting {
-  public static int selectionSort(int arr[]) {
-		
-  }
+    public static int selectionSort(int arr[]) {
+		int n = arr.length;
+		int maxInd = 0;
+		for(int i = 0; i < n-1; i++) {
+			for(int j = 0; j < n-i-1; j++) {
+				maxInd = 0;
+				if(arr[j] > arr[maxInd]) {
+					maxInd = j;
+				}
+			}
+			int temp = arr[j];
+			arr[j] = arr[n-i-1];
+			arr[n-i-1] = temp;
+		}
+    }
 }
