2b2d2fc84b6b5089210bee3c29688de6019edfb0
Samit Das <40141963+samitdas2003@users.noreply.github.com>
2022-01-04 18:24:00 +0530

    Update bubbleSort.java

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -1,2 +1,23 @@
-class BubbleSort {
+public class BubbleSorting {
+	
+	public void bubbleSort(int arr[]) {
+		int n = arr.length;
+		for(int i = 0; i < n-1; i++) {
+			boolean flag = true;
+			for(int j = 0; j < n-i-1; j++) {
+				if(arr[j+1] < arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+					flag = false;
+				}
+			}
+			if(flag) {
+				return;
+			}
+		}
 	}
+	
+	
+	
+}
